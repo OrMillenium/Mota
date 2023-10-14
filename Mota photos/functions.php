85541d2802ad638e657a0447a1c94cd0801dd5ce
aurore <aurore.esslinger@gmail.com>
2023-10-14 11:30:48 +0200

    functions cpt photos

--- Mota photos/functions.php
+++ Mota photos/functions.php
@@ -19,9 +19,54 @@ add_theme_support( 'post-thumbnails' );
 // Ajouter automatiquement le titre du site dans l'en-tête du site
 add_theme_support( 'title-tag' );
 
+
 //Ajout menu principal et menu footer
     register_nav_menu( 'main-menu', __( 'Menu principal', 'mota' ) );
     register_nav_menu( 'footer-menu', __( 'Menu footer', 'mota' ) );
+
+
+// Affichage CPT Photo
+$labels = array(
+    'name' => 'Photos',
+    'singular_name' => 'Photo',
+    'add_new_item' => 'Ajouter une photo',
+    'edit_item' => 'Modifier la photo',
+    'new_item' => 'Nouvelle photo',
+    'view_item' => 'Voir la photo',
+    'search_items' => 'Rechercher parmi les photos',
+    'not_found' => 'Aucune photo trouvée',
+    'not_found_in_trash' => 'Aucune photo trouvée dans la corbeille',
+);
+
+$args = array(
+    'labels' => $labels,
+    'public' => true,
+    'show_in_rest' => true,
+    'has_archive' => true,
+    'supports' => array( 'title', 'editor', 'thumbnail' ),
+    'menu_position' => 5,
+    'menu_icon' => 'dashicons-camera',
+    
+);
+
+register_post_type( 'photo', $args );
+}
+
+//Récupération du custom type photos
+function photos_request(){
+    $query = new WP_QUery([
+    'post_type' => 'photo',
+    'posts_per_page' => 10
+    ]);
+    
+    if($query->have_posts()) {
+        wp_send_json($query);
+    } else{
+        wp_send_json(false);
+    }
+    wp_die();
 }
 
 add_action( 'after_setup_theme', 'register_my_supports' );
+add_action('wp_ajax_request_photos','photos_request');
+add_action('wp_ajax_nopriv_request_photos','photos_request');
