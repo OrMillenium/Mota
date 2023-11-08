a388d56c82fa9d7bf3da653eb388d7c501f4a4b4
aurore <aurore.esslinger@gmail.com>
2023-11-08 13:50:16 +0100

    correction sur les champs des filtres

--- Mota photos/front-page.php
+++ Mota photos/front-page.php
@@ -39,7 +39,8 @@
         if ($terms && !is_wp_error($terms)) {
 
             echo "<select id='$taxonomy_slug' class='custom-select'>";
-            echo "<option value='' disabled selected class='defaultOption'>$label</option>";
+            // Option par défaut sans l'attribut disabled pour pouvoir y revenir
+            echo "<option value=''>$label</option>"; 
             foreach ($terms as $term) {
                 echo "<option value='$term->slug' class='term-option'>$term->name</option>";
             }
@@ -55,32 +56,34 @@
 
 
 
+
 <section id="photos-container" class="catalogue_block">
 
-<?php
-// Récupération de 8 photos aléatoires pour le bloc initial
-$args = array(
-    'post_type' => 'photo',
-    'posts_per_page' => 8,
-    'orderby' => 'date',
-    'order' => 'ASC',
-);
-$photo_block = new WP_Query($args);
+    <?php
+     // Récupération de 8 photos aléatoires pour le bloc initial
+          $args = array(
+             'post_type' => 'photo',
+             'posts_per_page' => 8,
+              'orderby' => 'date',
+             'order' => 'ASC',
+            );
+     $photo_block = new WP_Query($args);
 
-// Ajout de la variable globale pour le script AJAX
-wp_localize_script('custom-ajax-load', 'ajaxloadmore', array(
-    'ajaxurl' => admin_url('admin-ajax.php'),
-    'query_vars' => json_encode($args)
-));
+    // Ajout de la variable globale pour le script AJAX
+      wp_localize_script('custom-ajax-load', 'ajaxloadmore', array(
+                             'ajaxurl' => admin_url('admin-ajax.php'),
+                             'query_vars' => json_encode($args)
+                             )
+                        );
 
-if ($photo_block->have_posts()) :
+        if ($photo_block->have_posts()) :
     
         set_query_var('photo_block_args', array('context' => 'front-page'));
         while ($photo_block->have_posts()) :
             $photo_block->the_post();
         get_template_part('template-parts/photo_block', get_post_format());?>
 
-<?php
+    <?php
         endwhile; 
         wp_reset_postdata(); 
     else :
@@ -90,10 +93,10 @@ if ($photo_block->have_posts()) :
 
 
 
-<!-- Bouton Charger plus -->
-<div id="photo-block-more">
-<button id="load-more" data-page="1" data-url="<?php echo admin_url('admin-ajax.php'); ?>">Charger plus</button>
-</div>
+    <!-- Bouton Charger plus -->
+     <div id="photo-block-more">
+     <button id="load-more" data-page="1" data-url="<?php echo admin_url('admin-ajax.php'); ?>">Charger plus</button>
+     </div>
 
 </section>
 <?php get_footer(); ?>
